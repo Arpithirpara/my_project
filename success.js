73f97bec04fd97831d348879819fa73239affcfc
Arpit hirpara <arpithirapara27@gmail.com>
2026-03-21 18:36:18 +0530

    Update success.js

--- success.js
+++ success.js
@@ -1,12 +1,10 @@
-
-if(!JSON.parse(localStorage.getItem("paymentInfo"))){
+if(!localStorage.getItem("paymentInfo")){
     window.location.replace("index.html");
 }
 
-// baaki purana code neeche rahega...
-let payment = JSON.parse(localStorage.getItem("paymentInfo"));
 let payment = JSON.parse(localStorage.getItem("paymentInfo"));
 let cartItems = JSON.parse(localStorage.getItem("lastCartItems"));
+
 if(payment){
     let deliveryTime = new Date().getTime() + (10 * 60 * 1000);
     localStorage.setItem("deliveryTime", deliveryTime);
@@ -16,21 +14,20 @@ if(payment){
         <p><strong>Date:</strong> ${payment.date}</p>
         <p><strong>Status:</strong> ${payment.status}</p>
     `;
-     localStorage.removeItem("paymentInfo");
+    localStorage.removeItem("paymentInfo");
     localStorage.removeItem("lastCartItems");
 }
+
 document.addEventListener("DOMContentLoaded", function() {
     const sound = document.getElementById("orderSound");
-    
     sound.play().catch(() => {
-        console.log("Autoplay blocked. Waiting for user interaction...");
-        
         document.addEventListener("click", function playOnce() {
             sound.play();
             document.removeEventListener("click", playOnce);
         });
     });
 });
+
 if(cartItems){
     let html = "";
     cartItems.forEach(item => {
@@ -38,6 +35,7 @@ if(cartItems){
     });
     document.getElementById("itemList").innerHTML = html;
 }
+
 function goHome(){
     window.location.replace("index.html");
 }
