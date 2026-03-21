e60bda8cc137c44cdfeed5f9b254f56f713f5f7b
Arpit hirpara <arpithirapara27@gmail.com>
2026-03-21 18:58:58 +0530

    Update account.js

--- account.js
+++ account.js
@@ -1,16 +1,18 @@
+window.addEventListener("pageshow", function(event) {
+    if(event.persisted || localStorage.getItem("isLoggedIn") !== "true"){
+        window.location.replace("grocery.html");
+    }
+});
+
 window.onload = function () {
   let isLoggedIn = localStorage.getItem("isLoggedIn");
-
   if (isLoggedIn !== "true") {
     window.location.replace("grocery.html");
   }
 };
-
 function logout() {
-  console.log("logout clicked"); // debug
-
+  console.log("logout clicked");
   localStorage.removeItem("isLoggedIn");
   localStorage.removeItem("userName");
-
-  window.location.href = "login.html";
+  window.location.replace("login.html");
 }
